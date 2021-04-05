d54bdbb946c3ccb1ee3c0cc2d80ee51c48cea890
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2021-04-05 20:37:53 +0200

    fixing popup modal header overflow

--- public/js/home.js
+++ public/js/home.js
@@ -6,4 +6,32 @@ $(document).ready(() => {
         closeCTA();
     });
 
+    // ****************************************************************************************
+    // Count up feature on scroll
+    // ****************************************************************************************
+
+    var statsTopOffset = $("#stats").offset().top;
+    var countUpDone = false;
+
+    $(window).scroll(() => {
+
+        if ($(window).scrollTop() === 0) {
+            $("#go-up").hide(200);
+        }
+        else {
+            $("#go-up").show(200);
+        }
+
+        if (!countUpDone && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+            $(".counter").each(function () {
+                var counterElement = $(this);
+                var elementValue = parseInt(counterElement.text());
+                $(this).countup(elementValue);
+            })
+            countUpDone = true;
+        }
+    });
+
+    // ***************************************************************************************************
+
 })
